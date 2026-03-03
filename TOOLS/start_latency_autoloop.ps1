85727cd23fc585f099c098af04aa0974711482ac
skitela <skitela@gmail.com>
2026-03-03 23:44:00 +0100

    Fix autoloop launcher stdout/stderr redirection

--- TOOLS/start_latency_autoloop.ps1
+++ TOOLS/start_latency_autoloop.ps1
@@ -17,6 +17,7 @@ $runDir = Join-Path $rootResolved "RUN"
 New-Item -ItemType Directory -Path $runDir -Force | Out-Null
 $stamp = (Get-Date).ToUniversalTime().ToString("yyyyMMddTHHmmssZ")
 $logPath = Join-Path $runDir ("latency_autoloop_" + $stamp + ".log")
+$errPath = Join-Path $runDir ("latency_autoloop_" + $stamp + ".err.log")
 $pidPath = Join-Path $runDir "latency_autoloop_pid.txt"
 
 $args = @(
@@ -31,8 +32,7 @@ $args = @(
 if($RetentionBeforeLoop){ $args += "--retention-before-loop" }
 if($RetentionEvery -gt 0){ $args += @("--retention-every", [string]$RetentionEvery) }
 
-$proc = Start-Process -FilePath "python" -ArgumentList $args -WorkingDirectory $rootResolved -RedirectStandardOutput $logPath -RedirectStandardError $logPath -PassThru -WindowStyle Hidden
+$proc = Start-Process -FilePath "python" -ArgumentList $args -WorkingDirectory $rootResolved -RedirectStandardOutput $logPath -RedirectStandardError $errPath -PassThru -WindowStyle Hidden
 
 [string]$proc.Id | Set-Content -Path $pidPath -Encoding UTF8
-Write-Output ("LATENCY_AUTOLOOP_STARTED pid={0} log={1} pid_file={2}" -f $proc.Id, $logPath, $pidPath)
-
+Write-Output ("LATENCY_AUTOLOOP_STARTED pid={0} log={1} err={2} pid_file={3}" -f $proc.Id, $logPath, $errPath, $pidPath)
